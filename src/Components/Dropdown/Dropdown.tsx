33e9bc4175d3b82090487d6cd3080366c79c8d3d
facosta <facosta@eventbrite.com>
2023-03-23 20:04:35 -0300

    Dropdown component completed

--- src/Components/Dropdown/Dropdown.tsx
+++ src/Components/Dropdown/Dropdown.tsx
@@ -1,10 +1,78 @@
 import './Dropdown.scss';
 import { ArrowDownIcon } from '../../Icons/ArrowDown';
+import { CheckmarkIcon } from '../../Icons/Checkmark';
+import { Dispatch, SetStateAction, useState } from 'react';
 
-export const Dropdown = () => {
+const Option = ({
+    optionText,
+    currentOption,
+    selectedOption,
+    setSelectedOption,
+}: {
+    optionText: string;
+    currentOption: number;
+    selectedOption: number;
+    setSelectedOption: Dispatch<SetStateAction<number>>;
+}) => {
+    const isSelected = currentOption === selectedOption;
+    const selectCurrentOption = () => {
+        setSelectedOption(currentOption);
+    };
     return (
-        <button className="dropdown-button-container white">
-            VER: <span className="bold">POPULARES</span> <ArrowDownIcon />
-        </button>
+        <div
+            className={`dropdown-content-option white ${
+                isSelected && 'selected bold'
+            }`}
+            onClick={selectCurrentOption}>
+            <p>{optionText}</p>
+            <CheckmarkIcon />
+        </div>
+    );
+};
+
+const triangleFigure = (
+    <div className="triangle-whole">
+        <div className="triangle-left" />
+        <div className="triangle-right" />
+    </div>
+);
+export const Dropdown = () => {
+    const [selectedOption, setSelectedOption] = useState(0);
+    const [dropdownContentVisible, setDropdownContentVisible] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownContentVisible(!dropdownContentVisible);
+    };
+    const options = ['Populares', 'Mis peliculas'];
+
+    const dropdownContent = (
+        <>
+            <div className="dropdown-arrow-row">{triangleFigure}</div>
+            <div className="dropdown-content">
+                <div className="dropdown-content-options-container">
+                    {options.map((optionText, index) => {
+                        return (
+                            <Option
+                                optionText={optionText}
+                                key={index}
+                                currentOption={index}
+                                selectedOption={selectedOption}
+                                setSelectedOption={setSelectedOption}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        </>
+    );
+    return (
+        <div className="dropdown-container">
+            <button
+                className="dropdown-button-container white"
+                onClick={toggleDropdown}>
+                VER: <span className="bold">POPULARES</span> <ArrowDownIcon />
+            </button>
+            {dropdownContentVisible && dropdownContent}
+        </div>
     );
 };
---
+++ src/Icons/Checkmark/CheckmarkIcon.tsx
@@ -0,0 +1,6 @@
+import checkmarkSvg from './Checkmark.svg';
+import './CheckmarkIcon.scss';
+
+export const CheckmarkIcon = () => (
+    <img className="checkmark-icon" src={checkmarkSvg} alt="Checkmark icon" />
+);
